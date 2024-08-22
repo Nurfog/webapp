75993ca1355ebca02bd255ae389dd3cb3548f763
Juan Enrique Allende <juan.allende@gmail.com>
2024-08-22 17:24:29 -0400

    v0.0.1

--- web/web/urls.py
+++ web/web/urls.py
@@ -18,7 +18,17 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from . import views
+
+handle400 = views.error_404
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('courses/', views.courses, name='courses'),
+    path('team/', views.team, name='team'),
+    path('testimonial/', views.testimonial, name='testimonial'),
+    path('error_404/', views.error_404, name='error_404'),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
